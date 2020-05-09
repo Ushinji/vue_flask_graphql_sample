0c187b717459cc0014cb24a70cb05e66e540af29
Ushinji <siguma971@gmail.com>
2020-05-09 16:16:47 +0900

    Add allUsers, allProjects Query

--- app/schema/__init__.py
+++ app/schema/__init__.py
@@ -2,15 +2,13 @@ import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyConnectionField
 from .project import Project
+from .user import User
 
 
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
-    projects = graphene.List(Project)
-
-    def resolve_projects(self, info):
-        query = Project.get_query(info)  # SQLAlchemy query
-        return query.all()
+    all_users = SQLAlchemyConnectionField(User)
+    all_projects = SQLAlchemyConnectionField(Project)
 
 
 schema = graphene.Schema(query=Query)
