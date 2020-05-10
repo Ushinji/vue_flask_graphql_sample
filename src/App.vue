528028934b4deb5231fc828b0614c24abfec9751
Ushinji <siguma971@gmail.com>
2020-05-10 23:30:08 +0900

    Add vue/composition-api

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>Hello world</div>
+  <div>
+    <div>Hello world</div>
+    <p>{{ count }}</p>
+    <button @click="increment">+</button>
+  </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'App',
-};
+import { defineComponent, ref } from '@vue/composition-api';
+
+export default defineComponent({
+  setup() {
+    const count = ref(0);
+    const increment = () => {
+      count.value = count.value + 1;
+    };
+    return { count, increment };
+  },
+});
 </script>
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import Vue from 'vue';
+import VueCompositionApi from '@vue/composition-api';
 import App from './App';
+
+Vue.use(VueCompositionApi);
 
 const app = new Vue({
   render: (h) => h(App),
