6e140f744bc8bff42b7e7ae942b6e6e5b675a039
Ushinji <siguma971@gmail.com>
2020-05-10 23:35:15 +0900

    Setup graphql, vue-apollo

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
 import Vue from 'vue';
-import VueCompositionApi from '@vue/composition-api';
+import VueCompositionApi, { provide } from '@vue/composition-api';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import VueApollo from 'vue-apollo';
+import { DefaultApolloClient } from '@vue/apollo-composable';
 import App from './App';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql',
+});
+const cache = new InMemoryCache();
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+Vue.use(VueApollo);
 
 Vue.use(VueCompositionApi);
 
 const app = new Vue({
+  setup() {
+    provide(DefaultApolloClient, apolloClient);
+    return {};
+  },
   render: (h) => h(App),
 });
 
